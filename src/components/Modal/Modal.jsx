0c46442436d901d25c0855db9141d53fec3d6c31
Gary <vesperocyon@gmail.com>
2022-05-18 16:33:24 +0200

    Correction suite à la soutenance

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,19 @@
-import "./index.css"
-import React from "react";
+import './index.css'
+import React from 'react'
 
-const Modal = ({ isShowing, close }) => {
-   return( 
-       <div className={`background-closed ${isShowing ? "background-opened" : ""}`}>
-                    <div className="modal">
-                        <p className="modal-text">Employee Created!</p>
-                        <button className="modal-close" onClick={() => close()}>x</button>
-                </div>
-        </div>
-   )
+const Modal = ({ isShowing, close, text }) => {
+  return (
+    <div
+      className={`background-closed ${isShowing ? 'background-opened' : ''}`}
+    >
+      <div className="modal">
+        <p className="modal-text">{text}</p>
+        <button className="modal-close" onClick={() => close()}>
+          x
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Modal
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,121 +1,134 @@
-import { Link } from "react-router-dom";
-import React, { useState, useRef} from "react";
-import DatePicker from "react-datepicker";
-import { states } from "../../data/states";
-import { departments } from "../../data/departments";
-import "react-datepicker/dist/react-datepicker.css";
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
-import { useDispatch } from "react-redux";
-import { pushEmployee } from "../../redux/Reducer/slice";
-import Modal from "../../components/Modal/Modal.jsx";
+import { Link } from 'react-router-dom'
+import React, { useState, useRef } from 'react'
+import DatePicker from 'react-datepicker'
+import { states } from '../../data/states'
+import { departments } from '../../data/departments'
+import 'react-datepicker/dist/react-datepicker.css'
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
+import { useDispatch } from 'react-redux'
+import { pushEmployee } from '../../redux/Reducer/slice'
+import Modal from '../../components/Modal/Modal.jsx'
 
 function Home() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-    const [isShowing, setIsShowing] = useState(false);
+  const text = 'Employee created!'
 
-    const firstNameInput = useRef();
-    const lastNameInput = useRef();
-    const streetInput = useRef();
-    const cityInput = useRef();
-    const zipInput = useRef();
+  const [isShowing, setIsShowing] = useState(false)
 
-    const [birthDate, setBirthDate] = useState(null);
-    const [startDate, setStartDate] = useState(null);
+  const firstNameInput = useRef()
+  const lastNameInput = useRef()
+  const streetInput = useRef()
+  const cityInput = useRef()
+  const zipInput = useRef()
 
-    const statesOptions = states
-    const [state, setState] = useState(states[0]);
+  const [birthDate, setBirthDate] = useState(null)
+  const [startDate, setStartDate] = useState(null)
 
-    const departmentsOptions = departments
-    const [department, setDepartment] = useState(departments[0]);
+  const statesOptions = states
+  const [state, setState] = useState(states[0])
 
+  const departmentsOptions = departments
+  const [department, setDepartment] = useState(departments[0])
 
-    const onEditState = (value) => {
-        setState(value);
+  const onEditState = (value) => {
+    setState(value)
+  }
+
+  const onEditDepartment = (value) => {
+    setDepartment(value)
+  }
+
+  const handleSubmit = (e) => {
+    const employee = {
+      id: Date.now(),
+      firstName: firstNameInput.current.value,
+      lastName: lastNameInput.current.value,
+      birthDate: JSON.stringify(birthDate).substring(1, 11),
+      startDate: JSON.stringify(startDate).substring(1, 11),
+      street: streetInput.current.value,
+      city: cityInput.current.value,
+      zip: zipInput.current.value,
+      state: state.value,
+      department: department.value,
     }
+    let employeeArray = Object.values(employee)
 
-    const onEditDepartment = (value) => {
-        setDepartment(value)
+    if (!employeeArray.some((element) => element === '' || element === 'ull')) {
+      dispatch(pushEmployee(employee))
+      setIsShowing(true)
     }
+  }
 
-    const handleSubmit = (e) => {
+  return (
+    <main>
+      <div className="title">
+        <h1>HRnet</h1>
+      </div>
+      <div className="container">
+        <Link to="/employees-list">View Current Employees</Link>
+        <h2>Create Employee</h2>
+        <form action="#" id="create-employee">
+          <label htmlFor="first-name">First Name</label>
+          <input ref={firstNameInput} id="first-name" required="required" />
 
-        const employee = {
-            id : Date.now(),
-            firstName : firstNameInput.current.value,
-            lastName : lastNameInput.current.value,
-            birthDate : JSON.stringify(birthDate).substring(1,11),
-            startDate : JSON.stringify(startDate).substring(1,11),
-            street : streetInput.current.value,
-            city : cityInput.current.value,
-            zip : zipInput.current.value,
-            state : state.value,
-            department : department.value,
-        }
-        console.log(employee)
-        let employeeArray = Object.values(employee);
+          <label htmlFor="last-name">Last Name</label>
+          <input ref={lastNameInput} id="last-name" required="required" />
 
-        if (!employeeArray.some(element => element === '' || element === "ull")) {
-        dispatch(pushEmployee(employee))
-        setIsShowing(true);
-        }
-    }
+          <label htmlFor="date-of-birth">Date of Birth</label>
+          <DatePicker
+            selected={birthDate}
+            onChange={(date: Date) => setBirthDate(date)}
+            id="date-of-birth"
+            required="required"
+          />
 
-    return(
-        <main>
-            <div className="title">
-                <h1>HRnet</h1>
-            </div>
-            <div className="container">
-                <Link to="/employees-list">View Current Employees</Link>
-                <h2>Create Employee</h2>
-                <form action="#" id="create-employee">
-                    <label htmlFor="first-name">First Name</label>
-                    <input ref={firstNameInput} id="first-name" required="required"/>
+          <label htmlFor="start-date">Start Date</label>
+          <DatePicker
+            selected={startDate}
+            onChange={(date: Date) => setStartDate(date)}
+            id="start-date"
+            required="required"
+          />
 
-                    <label htmlFor="last-name">Last Name</label>
-                    <input ref={lastNameInput} id="last-name" required="required"/>
+          <fieldset className="address">
+            <legend>Address</legend>
 
-                    <label htmlFor="date-of-birth">Date of Birth</label>
-                    <DatePicker selected={birthDate} onChange={(date:Date) => setBirthDate(date)} id="date-of-birth" required="required" />
+            <label htmlFor="street">Street</label>
+            <input ref={streetInput} id="street" required="required" />
 
-                    <label htmlFor="start-date">Start Date</label>
-                    <DatePicker selected={startDate} onChange={(date:Date) => setStartDate(date)} id="start-date" required="required" />
+            <label htmlFor="city">City</label>
+            <input ref={cityInput} id="city" required="required" />
 
-                    <fieldset className="address">
-                        <legend>Address</legend>
+            <label htmlFor="state">State</label>
+            <Dropdown
+              options={statesOptions}
+              onChange={onEditState}
+              value={state}
+            />
+            <label htmlFor="zip-code">Zip Code</label>
+            <input ref={zipInput} id="zip-code" required="required" />
+          </fieldset>
 
-                        <label htmlFor="street">Street</label>
-                        <input ref={streetInput} id="street" required="required" />
+          <label htmlFor="department">Department</label>
+          <Dropdown
+            options={departmentsOptions}
+            onChange={onEditDepartment}
+            value={department}
+          />
+        </form>
 
-                        <label htmlFor="city">City</label>
-                        <input ref={cityInput} id="city" required="required" />
+        <Modal
+          isShowing={isShowing}
+          close={() => setIsShowing(false)}
+          text={text}
+        />
 
-                        <label htmlFor="state">State</label>
-                        <Dropdown
-                            options={statesOptions}
-                            onChange={onEditState}
-                            value={state}
-                        />
-                        <label htmlFor="zip-code">Zip Code</label>
-                        <input ref={zipInput} id="zip-code" required="required"/>
-                    </fieldset>
-
-                    <label htmlFor="department">Department</label>
-                        <Dropdown
-                            options={departmentsOptions}
-                            onChange={onEditDepartment}
-                            value={department}
-                        />
-                </form>
-
-                <Modal isShowing={isShowing} close={() => setIsShowing(false)}/>
-
-                <button onClick={handleSubmit} >Save</button>
-            </div>
-        </main>
-    )
+        <button onClick={handleSubmit}>Save</button>
+      </div>
+    </main>
+  )
 }
 
 export default Home
